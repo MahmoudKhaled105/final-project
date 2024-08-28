75d55ee7162d272fc98fe3d707d473d6e997defe
MahmoudKhaled <mk7519658@gmail.com>
2024-08-29 00:26:26 +0300

    firstCommit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component:AuthLayoutComponent, children: [
+        {path:'register', component:RegisterComponent}
+
+    ],},
+
+    
+    {path:'home', component:HomeComponent}
+
+
+];
