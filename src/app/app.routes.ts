f78eede19b2ec811ab42b0307148c814a18d2b2c
MahmoudKhaled <mk7519658@gmail.com>
2024-08-30 05:56:54 +0300

    loginForm

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,18 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
-    {path:'', component:AuthLayoutComponent, children: [
-        {path:'register', component:RegisterComponent}
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      { path: '', redirectTo: 'register', pathMatch: 'full' },
+      { path: 'register', component: RegisterComponent },
+      {path:'login', component:LoginComponent}
+    ],
+  },
 
-    ],},
-
-    
-    {path:'home', component:HomeComponent}
-
-
+  { path: 'home', component: HomeComponent },
 ];
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavBlankComponent } from "../nav-blank/nav-blank.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [NavBlankComponent, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
