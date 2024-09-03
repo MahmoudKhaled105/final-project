e1d65d5a51b433d3441e3fe3389d8a27de134ef7
Amr <amrhamed858@gmail.com>
2024-09-03 18:09:41 +0300

    app_route fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,20 +2,7 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
-<<<<<<< HEAD
 import { SellerAccountComponent } from './components/seller-account/seller-account.component';
-
-export const routes: Routes = [
-    {path:'', component:AuthLayoutComponent, children: [
-        {path:'register', component:RegisterComponent}
-
-    ],},
-
-    
-    {path:'home', component:HomeComponent},
-    {path:'seller-account', component:SellerAccountComponent}
-
-=======
 import { LoginComponent } from './components/login/login.component';
 import { BlankLayoutComponent } from './layouts/blank-layout/blank-layout.component';
 
@@ -29,7 +16,6 @@ export const routes: Routes = [
       { path: 'login', component: LoginComponent },
     ],
   },
->>>>>>> 31ab83715362fcbdcb81ee6c6d4d602c224ddd6d
 
   {
     path: '',
@@ -37,6 +23,7 @@ export const routes: Routes = [
     children: [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', component: HomeComponent },
+      { path: 'seller-account', component: SellerAccountComponent },
     ],
   },
 ];
