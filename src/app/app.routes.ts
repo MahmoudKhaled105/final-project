69fface24c5f1a008379a68104904eeff20a2f96
MahmoudKhaled <mk7519658@gmail.com>
2024-09-03 18:09:22 +0300

    edit-seller-account

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,22 +2,11 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
-<<<<<<< HEAD
 import { SellerAccountComponent } from './components/seller-account/seller-account.component';
-
-export const routes: Routes = [
-    {path:'', component:AuthLayoutComponent, children: [
-        {path:'register', component:RegisterComponent}
-
-    ],},
-
-    
-    {path:'home', component:HomeComponent},
-    {path:'seller-account', component:SellerAccountComponent}
-
-=======
 import { LoginComponent } from './components/login/login.component';
 import { BlankLayoutComponent } from './layouts/blank-layout/blank-layout.component';
+
+
 
 export const routes: Routes = [
   {
@@ -29,14 +18,14 @@ export const routes: Routes = [
       { path: 'login', component: LoginComponent },
     ],
   },
->>>>>>> 31ab83715362fcbdcb81ee6c6d4d602c224ddd6d
-
+ 
   {
     path: '',
     component: BlankLayoutComponent,
     children: [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', component: HomeComponent },
+      { path: 'seller-account', component: SellerAccountComponent },
     ],
   },
 ];
