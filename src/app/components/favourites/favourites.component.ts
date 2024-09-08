17423519d3f3be6e34121cb8f0ed579f4c5cbd15
Amr <amrhamed858@gmail.com>
2024-09-08 18:49:16 +0300

    FavouritesWithSliderEddition

--- src/app/components/favourites/favourites.component.ts
+++ src/app/components/favourites/favourites.component.ts
@@ -1,19 +1,20 @@
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-favourites',
   standalone: true,
-  imports: [CarouselModule],
+  imports: [CarouselModule, CommonModule, NgFor],
   templateUrl: './favourites.component.html',
   styleUrls: ['./favourites.component.scss']  // Corrected this line
 })
 export class FavouritesComponent {
 
-  images: string[] = [
-    "../../../assets/images/chewy-fGxiRXr2oZg-unsplash.jpg",
-    "../../../assets/images/chewy-fGxiRXr2oZg-unsplash.jpg",
-    "../../../assets/images/chewy-fGxiRXr2oZg-unsplash.jpg"
+  images: object[] = [
+  { src :"../../../assets/images/chewy-fGxiRXr2oZg-unsplash.jpg"},
+  { src :"../../../assets/images/corinne-kutz-j_9drN8w6gw-unsplash.jpg.jpg"},
+  { src :"../../../assets/images/joanna-nix-walkup-h3stFPAyn7E-unsplash.jpg"},
   ];
 
   imgSlider: OwlOptions = {
@@ -22,7 +23,7 @@ export class FavouritesComponent {
     touchDrag: true,
     pullDrag: false,
     dots: true,
-    autoplay: true,
+    autoplay: false,
     autoplayTimeout: 2000,
     navSpeed: 700,
     items: 1,
